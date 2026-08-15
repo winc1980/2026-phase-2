f51064b5c66f7bb520517636c4f77b648209b617
hayafas <y20342634@gmail.com>
2026-08-15 15:24:04 +0900

    コンポーネントの中に、issue 2関連のことをまとめたLogin.tsxを作ることによって、適切に動作されるようになりました。

---
+++ app/components/common/Login.tsx
@@ -0,0 +1,26 @@
+import { Button } from "../ui/button";
+import { Link } from "react-router";
+
+export function Login() {
+  return (
+    <Button variant="brand" size="xl" className="rounded-2xl" asChild>
+      <Link to="/app/live/create">ライブを作成する</Link>
+    </Button>
+  );
+}
+
+export function Register() {
+  return (
+    <Button variant="brand" size="xl" className="rounded-2xl" asChild>
+      <Link to="/auth/register">新規アカウント登録</Link>
+    </Button>
+  );
+}
+
+export function Home() {
+  return (
+    <Button variant="brand" size="xl" className="rounded-2xl" asChild>
+      <Link to="/app">ホームに移動</Link>
+    </Button>
+  );
+}
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,17 +1,23 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import { Register } from "~/components/common/Login";
+import { Login } from "~/components/common/Login";
+import { Home } from "~/components/common/Login";
 
 export default function TopPage() {
-	const [message, setMessage] = useState<string | null>(null)
-	useEffect(() => {
-		fetch(`${window.location.origin}/api/hello-react-router`)
-			.then((res) => res.json())
-			.then((json) => setMessage(json.message))
-	}, [])
+  const [message, setMessage] = useState<string | null>(null);
+  useEffect(() => {
+    fetch(`${window.location.origin}/api/hello-react-router`)
+      .then((res) => res.json())
+      .then((json) => setMessage(json.message));
+  }, []);
 
-	return (
-		<div>
-			<h1>トップページ</h1>
-			<div>{message}</div>
-		</div>
-	)
+  return (
+    <div>
+      <h1>トップページ</h1>
+      <div>{message}</div>
+      <Login></Login>
+      <Register />
+      <Home />
+    </div>
+  );
 }

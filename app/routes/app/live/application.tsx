96549b308bbf4875a1047ba449c04314ab3a8c65
hayafas <y20342634@gmail.com>
2026-08-18 10:12:00 +0900

    ㊗️コンポーネントでの分割の1つ目が完了！！

--- app/routes/app/live/application.tsx
+++ app/routes/app/live/application.tsx
@@ -97,11 +97,7 @@ export async function loader({ context, request }: Route.LoaderArgs) {
 export default function LiveApplicationPage({
 	loaderData: { availableApplicationsWithUrl, suspendedApplicationsWithUrl },
 }: Route.ComponentProps) {
-	const [openCreateDialog, setOpenCreateDialog] = useState(false)
-	const [initialAvailability, setInitialAvailability] = useState(false)
 
-	const formRef = useRef<HTMLFormElement>(null)
-	const fetcher1 = useFetcher()
 
 	const fetcher2 = useFetcher()
 
@@ -124,102 +120,7 @@ export default function LiveApplicationPage({
 					参加を希望するバンドにこのURLを共有してください
 				</p>
 			</div>
-			<Collapsible>
-				<Card>
-					<CardHeader>
-						<CardTitle className="flex justify-between items-center">
-							<div>新しいバンド募集リンクを作成</div>
-							<CollapsibleTrigger asChild>
-								<Button size="icon" variant="ghost">
-									<ChevronsUpDownIcon />
-								</Button>
-							</CollapsibleTrigger>
-						</CardTitle>
-					</CardHeader>
-					<CollapsibleContent asChild>
-						<CardContent>
-							<fetcher1.Form method="POST" ref={formRef}>
-								<input type="hidden" name="intent" value="create" />
-								<FieldGroup>
-									<Field>
-										<FieldLabel htmlFor="application-name">
-											募集分類名（任意）
-										</FieldLabel>
-										<FieldDescription>
-											ライブ管理者の識別のためにのみ使用します。外部へ公開されません。
-										</FieldDescription>
-										<Input
-											id="application-name"
-											name="application-name"
-											placeholder="例：一次募集"
-										/>
-									</Field>
-									<Field>
-										<FieldLabel>初期設定</FieldLabel>
-										<div className="inline-flex gap-2">
-											<Switch
-												id="available"
-												checked={initialAvailability}
-												onCheckedChange={setInitialAvailability}
-											/>
-											<input
-												type="hidden"
-												name="initial-available"
-												value={Number(initialAvailability)}
-											/>
-											<FieldLabel htmlFor="available">停止・有効</FieldLabel>
-										</div>
-									</Field>
-									<Field>
-										<Button
-											type="button"
-											className="w-full"
-											onClick={() => setOpenCreateDialog(true)}
-											disabled={fetcher1.state === "submitting"}
-										>
-											{fetcher1.state === "submitting" ? (
-												<LoaderCircleIcon className="animate-spin" />
-											) : (
-												<SendIcon />
-											)}
-											作成
-										</Button>
-									</Field>
-								</FieldGroup>
-							</fetcher1.Form>
-						</CardContent>
-					</CollapsibleContent>
-				</Card>
-				<AlertDialog open={openCreateDialog} onOpenChange={setOpenCreateDialog}>
-					<AlertDialogContent>
-						<AlertDialogHeader>
-							<AlertDialogTitle>
-								{initialAvailability
-									? "本当に募集を始めてもいいですか？"
-									: "募集リンクを作成します。"}
-							</AlertDialogTitle>
-							<AlertDialogDescription>
-								<span>
-									現在の初期設定は「{initialAvailability ? "有効" : "停止"}
-									」です。
-								</span>
-								<span>いつでも停止・再開できます。</span>
-							</AlertDialogDescription>
-						</AlertDialogHeader>
-						<AlertDialogFooter>
-							<AlertDialogCancel>キャンセル</AlertDialogCancel>
-							<AlertDialogAction
-								type="button"
-								onClick={() => {
-									if (formRef.current) fetcher1.submit(formRef.current)
-								}}
-							>
-								募集を始める
-							</AlertDialogAction>
-						</AlertDialogFooter>
-					</AlertDialogContent>
-				</AlertDialog>
-			</Collapsible>
+			<NewLinkForm />
 			<Card>
 				<CardHeader>
 					<CardTitle className="flex gap-1 items-center">
@@ -323,6 +224,120 @@ export default function LiveApplicationPage({
 			</Card>
 		</div>
 	)
+}
+
+function NewLinkForm(){
+	const [openCreateDialog, setOpenCreateDialog] = useState(false)
+	const [initialAvailability, setInitialAvailability] = useState(false)
+
+	const formRef = useRef<HTMLFormElement>(null)
+	const fetcher1 = useFetcher()
+	return(
+	<Collapsible>
+				<Card>
+					<CardHeader>
+						<CardTitle className="flex justify-between items-center">
+							<div>新しいバンド募集リンクを作成</div>
+							<CollapsibleTrigger asChild>
+								<Button size="icon" variant="ghost">
+									<ChevronsUpDownIcon />
+								</Button>
+							</CollapsibleTrigger>
+						</CardTitle>
+					</CardHeader>
+					<CollapsibleContent asChild>
+						<CardContent>
+							<fetcher1.Form method="POST" ref={formRef}>
+								<input type="hidden" name="intent" value="create" />
+								<FieldGroup>
+									<Field>
+										<FieldLabel htmlFor="application-name">
+											募集分類名（任意）
+										</FieldLabel>
+										<FieldDescription>
+											ライブ管理者の識別のためにのみ使用します。外部へ公開されません。
+										</FieldDescription>
+										<Input
+											id="application-name"
+											name="application-name"
+											placeholder="例：一次募集"
+										/>
+									</Field>
+									<Field>
+										<FieldLabel>初期設定</FieldLabel>
+										<div className="inline-flex gap-2">
+											<Switch
+												id="available"
+												checked={initialAvailability}
+												onCheckedChange={setInitialAvailability}
+											/>
+											<input
+												type="hidden"
+												name="initial-available"
+												value={Number(initialAvailability)}
+											/>
+											<FieldLabel htmlFor="available">停止・有効</FieldLabel>
+										</div>
+									</Field>
+									<Field>
+										<Button
+											type="button"
+											className="w-full"
+											onClick={() => setOpenCreateDialog(true)}
+											disabled={fetcher1.state === "submitting"}
+										>
+											{fetcher1.state === "submitting" ? (
+												<LoaderCircleIcon className="animate-spin" />
+											) : (
+												<SendIcon />
+											)}
+											作成
+										</Button>
+									</Field>
+								</FieldGroup>
+							</fetcher1.Form>
+						</CardContent>
+					</CollapsibleContent>
+				</Card>
+				<AlertDialog open={openCreateDialog} onOpenChange={setOpenCreateDialog}>
+					<AlertDialogContent>
+						<AlertDialogHeader>
+							<AlertDialogTitle>
+								{initialAvailability
+									? "本当に募集を始めてもいいですか？"
+									: "募集リンクを作成します。"}
+							</AlertDialogTitle>
+							<AlertDialogDescription>
+								<span>
+									現在の初期設定は「{initialAvailability ? "有効" : "停止"}
+									」です。
+								</span>
+								<span>いつでも停止・再開できます。</span>
+							</AlertDialogDescription>
+						</AlertDialogHeader>
+						<AlertDialogFooter>
+							<AlertDialogCancel>キャンセル</AlertDialogCancel>
+							<AlertDialogAction
+								type="button"
+								onClick={() => {
+									if (formRef.current) fetcher1.submit(formRef.current)
+								}}
+							>
+								募集を始める
+							</AlertDialogAction>
+						</AlertDialogFooter>
+					</AlertDialogContent>
+				</AlertDialog>
+			</Collapsible>
+)
+}
+
+function VerifiedLink(){
+
+}
+
+function PausedLink(){
+	
 }
 
 const FormDataSchema = v.variant("intent", [

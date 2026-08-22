1c49f8ec776e24e96c9e5e1edc71f333594c79d4
hayafas <y20342634@gmail.com>
2026-08-22 17:10:17 +0900

    作業途中

--- app/routes/app/live/application.tsx
+++ app/routes/app/live/application.tsx
@@ -94,136 +94,23 @@ export async function loader({ context, request }: Route.LoaderArgs) {
 	}
 }
 
-export default function LiveApplicationPage({
-	loaderData: { availableApplicationsWithUrl, suspendedApplicationsWithUrl },
-}: Route.ComponentProps) {
-
-
-	const fetcher2 = useFetcher()
-
-	const handleCopy = async (url: string) => {
-		const result = await wrapPromise(navigator.clipboard.writeText(url))
-		if (!result.success) {
-			showToast({
-				type: "error",
-				message: "クリップボードへのコピーが失敗しました",
-			})
-		}
-		showToast({ type: "success", message: "申請リンクをコピーしました" })
-	}
-
-	return (
-		<div className="space-y-6">
-			<div className="space-y-1">
-				<h1 className="text-2xl font-semibold">バンドの募集</h1>
-				<p className="text-muted-foreground text-sm">
-					参加を希望するバンドにこのURLを共有してください
-				</p>
-			</div>
-			<NewLinkForm />
-			<Card>
-				<CardHeader>
-					<CardTitle className="flex gap-1 items-center">
-						有効なリンク
-					</CardTitle>
-				</CardHeader>
-				<CardContent>
-					{availableApplicationsWithUrl.length === 0 ? (
-						<span className="text-muted-foreground text-sm">
-							有効なリンクはありません
-						</span>
-					) : (
-						<div className="space-y-4">
-							{availableApplicationsWithUrl.map((apl) => (
-								<div className="space-y-1" key={apl.id}>
-									<div className="w-full flex items-baseline gap-2">
-										<span className="shrink-0">{apl.name}</span>
-										<span className="shrink-0 text-muted-foreground text-xs">
-											{formatPlainDateTime(apl.updatedAt)}
-										</span>
-									</div>
-									<div className="flex gap-2 items-center">
-										<div className="grow truncate text-muted-foreground py-2 px-4 bg-muted rounded-lg">
-											{apl.url}
-										</div>
-										<Button
-											size="icon-lg"
-											variant="destructive"
-											onClick={() => {
-												const formData = new FormData()
-												formData.append("intent", "suspend-application")
-												formData.append("application-id", String(apl.id))
-												fetcher2.submit(formData, { method: "POST" })
-											}}
-										>
-											<PauseIcon />
-										</Button>
-										<Button
-											size="icon-lg"
-											className="w-16"
-											onClick={() => handleCopy(apl.url)}
-										>
-											<CopyIcon />
-										</Button>
-									</div>
-								</div>
-							))}
-						</div>
-					)}
-				</CardContent>
-				<Separator />
-				<CardHeader>
-					<CardTitle className="flex gap-1 items-center">
-						停止されたリンク
-					</CardTitle>
-				</CardHeader>
-				<CardContent>
-					{suspendedApplicationsWithUrl.length === 0 ? (
-						<span className="text-muted-foreground text-sm">
-							有効なリンクはありません
-						</span>
-					) : (
-						<div className="space-y-4">
-							{suspendedApplicationsWithUrl.map((apl) => (
-								<div className="space-y-1" key={apl.id}>
-									<div className="shrink-0 inline-flex w-24 gap-2">
-										<span className="shrink-0">{apl.name}</span>
-										<span className="shrink-0 text-muted-foreground">
-											{formatPlainDateTime(apl.updatedAt)}
-										</span>
-									</div>
-									<div className="flex gap-2 items-center">
-										<div className="grow truncate text-muted-foreground py-2 px-4 bg-muted rounded-lg">
-											{apl.url}
-										</div>
-										<Button
-											size="icon-lg"
-											variant="destructive"
-											onClick={() => {
-												const formData = new FormData()
-												formData.append("intent", "enable-application")
-												formData.append("application-id", String(apl.id))
-												fetcher2.submit(formData, { method: "POST" })
-											}}
-										>
-											<PlayIcon />
-										</Button>
-										<Button
-											size="icon-lg"
-											className="w-16"
-											onClick={() => handleCopy(apl.url)}
-										>
-											<CopyIcon />
-										</Button>
-									</div>
-								</div>
-							))}
-						</div>
-					)}
-				</CardContent>
-			</Card>
-		</div>
-	)
+export default function LiveApplicationPage() {
+  return (
+    <div className="space-y-6">
+      <div className="space-y-1">
+        <h1 className="text-2xl font-semibold">バンドの募集</h1>
+        <p className="text-muted-foreground text-sm">
+          参加を希望するバンドにこのURLを共有してください
+        </p>
+      </div>
+      <NewLinkForm />
+      <Card>
+        <VerifiedLink />
+        <Separator />
+        <PausedLink />
+      </Card>
+    </div>
+  );
 }
 
 function NewLinkForm(){
@@ -332,12 +219,143 @@ function NewLinkForm(){
 )
 }
 
-function VerifiedLink(){
+function VerifiedLink({
+  loaderData: { availableApplicationsWithUrl },
+}: Route.ComponentProps) {
+  const fetcher2 = useFetcher();
 
+  const handleCopy = async (url: string) => {
+    const result = await wrapPromise(navigator.clipboard.writeText(url));
+    if (!result.success) {
+      showToast({
+        type: "error",
+        message: "クリップボードへのコピーが失敗しました",
+      });
+    }
+    showToast({ type: "success", message: "申請リンクをコピーしました" });
+  };
+
+  return (
+    <>
+      <CardHeader>
+        <CardTitle className="flex gap-1 items-center">有効なリンク</CardTitle>
+      </CardHeader>
+      <CardContent>
+        {availableApplicationsWithUrl.length === 0 ? (
+          <span className="text-muted-foreground text-sm">
+            有効なリンクはありません
+          </span>
+        ) : (
+          <div className="space-y-4">
+            {availableApplicationsWithUrl.map((apl) => (
+              <div className="space-y-1" key={apl.id}>
+                <div className="w-full flex items-baseline gap-2">
+                  <span className="shrink-0">{apl.name}</span>
+                  <span className="shrink-0 text-muted-foreground text-xs">
+                    {formatPlainDateTime(apl.updatedAt)}
+                  </span>
+                </div>
+                <div className="flex gap-2 items-center">
+                  <div className="grow truncate text-muted-foreground py-2 px-4 bg-muted rounded-lg">
+                    {apl.url}
+                  </div>
+                  <Button
+                    size="icon-lg"
+                    variant="destructive"
+                    onClick={() => {
+                      const formData = new FormData();
+                      formData.append("intent", "suspend-application");
+                      formData.append("application-id", String(apl.id));
+                      fetcher2.submit(formData, { method: "POST" });
+                    }}
+                  >
+                    <PauseIcon />
+                  </Button>
+                  <Button
+                    size="icon-lg"
+                    className="w-16"
+                    onClick={() => handleCopy(apl.url)}
+                  >
+                    <CopyIcon />
+                  </Button>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </CardContent>
+    </>
+  );
 }
 
-function PausedLink(){
-	
+function PausedLink({
+  loaderData: { suspendedApplicationsWithUrl },
+}: Route.ComponentProps) {
+  const handleCopy = async (url: string) => {
+    const result = await wrapPromise(navigator.clipboard.writeText(url));
+    if (!result.success) {
+      showToast({
+        type: "error",
+        message: "クリップボードへのコピーが失敗しました",
+      });
+    }
+    showToast({ type: "success", message: "申請リンクをコピーしました" });
+  };
+  const fetcher2 = useFetcher();
+
+  return (
+    <>
+      <CardHeader>
+        <CardTitle className="flex gap-1 items-center">
+          停止されたリンク
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        {suspendedApplicationsWithUrl.length === 0 ? (
+          <span className="text-muted-foreground text-sm">
+            有効なリンクはありません
+          </span>
+        ) : (
+          <div className="space-y-4">
+            {suspendedApplicationsWithUrl.map((apl) => (
+              <div className="space-y-1" key={apl.id}>
+                <div className="shrink-0 inline-flex w-24 gap-2">
+                  <span className="shrink-0">{apl.name}</span>
+                  <span className="shrink-0 text-muted-foreground">
+                    {formatPlainDateTime(apl.updatedAt)}
+                  </span>
+                </div>
+                <div className="flex gap-2 items-center">
+                  <div className="grow truncate text-muted-foreground py-2 px-4 bg-muted rounded-lg">
+                    {apl.url}
+                  </div>
+                  <Button
+                    size="icon-lg"
+                    variant="destructive"
+                    onClick={() => {
+                      const formData = new FormData();
+                      formData.append("intent", "enable-application");
+                      formData.append("application-id", String(apl.id));
+                      fetcher2.submit(formData, { method: "POST" });
+                    }}
+                  >
+                    <PlayIcon />
+                  </Button>
+                  <Button
+                    size="icon-lg"
+                    className="w-16"
+                    onClick={() => handleCopy(apl.url)}
+                  >
+                    <CopyIcon />
+                  </Button>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </CardContent>
+    </>
+  );
 }
 
 const FormDataSchema = v.variant("intent", [
